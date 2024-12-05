529b123c82fad2af7592b961fbcbbf99a14d3940
Andreilomakin <andreilomakin00001983@gmail.com>
2024-12-05 22:36:00 +0300

    Add files via upload

---
+++ main.py2.py
@@ -0,0 +1,5 @@
+course='Python'
+in_total=12
+hours_spent=1.5
+on_average='среднее время на одно задание'
+print(course,'всего', in_total, 'задач, на них затрачено',hours_spent,'часа,',on_average,hours_spent/in_total,'часа')
