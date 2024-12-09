7a0320f52f5dd05462031f0a9566266d924e5bc4
Andreilomakin <andreilomakin00001983@gmail.com>
2024-12-10 00:07:14 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+my_string=input('Тяжело в учении, легко в программировании: ')
+print(len(my_string))
+print('Тяжело в учении, легко в программировании'.upper())
+print('Тяжело в учении, легко в программировании'.lower())
+print(my_string.replace(' ',''))
+print(my_string[0])
+print(my_string[-1])
