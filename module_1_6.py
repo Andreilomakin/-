a92f8503722b344e3d045ee3badf9493882040b0
Andreilomakin <andreilomakin00001983@gmail.com>
2024-12-11 21:26:27 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,17 @@
+my_dict={'Андрей':1983,'Елена':1985,'Игорь':1989}
+print(my_dict)
+print(my_dict['Андрей'])
+my_dict['Антон']=1990
+print(my_dict)
+my_dict['Марина']=1987
+my_dict['Александр']=1999
+print(my_dict)
+del my_dict['Игорь']
+print(my_dict)
+my_set={1,2,3,4,5,5,5,5,4,4,4,4,'Слива'}
+print(my_set)
+my_set.add('Яблоко')
+my_set.add(7)
+print(my_set)
+my_set.discard(3)
+print(my_set)
